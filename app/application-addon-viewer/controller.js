0e1416956ce590a99dfedf18f5372405122b03b0
Ilya Radchenko <ilya@burstcreations.com>
2018-12-11 12:24:52 -0500

    feat: add search

--- app/application-addon-viewer/controller.js
+++ app/application-addon-viewer/controller.js
@@ -1,4 +1,5 @@
 import Controller from '@ember/controller';
+import { A } from '@ember/array';
 import data from '../addon-data';
 
 export default Controller.extend({
@@ -6,7 +7,26 @@ export default Controller.extend({
 
   init() {
     this._super(...arguments);
-    let data = this.data;
-    this.set('addons', data.sortBy('name'));
+    let data = A(this.data);
+    let sorted = A(data.sortBy('name'));
+
+    this.set('addons', sorted);
+    this.set('filteredAddons', A([...sorted]));
+    this.set('uniqueAuthors', A(A(sorted.mapBy('author')).uniq()));
+  },
+
+  actions: {
+    filter(query) {
+      let filtered;
+
+      if (query) {
+        filtered = A(this.addons.filter(addon => addon.name.includes(query)));
+      } else {
+        filtered = A([...this.addons]);
+      }
+
+      this.set('filteredAddons', filtered);
+      this.set('uniqueAuthors', A(A(filtered.mapBy('author')).uniq()));
+    }
   }
 });
